5e795d49fc2b32429ab0266fbf181ee296bb1dfd
jessibmoura <jessicabmoura00@gmail.com>
2025-10-20 18:50:36 -0400

    notebook: initial data analysis on the complete dataset

--- data/get_data.py
+++ data/get_data.py
@@ -7,7 +7,8 @@ def main():
 
     # data (as pandas dataframes) 
     X = diabetes_130_us_hospitals_for_years_1999_2008.data.features 
-    y = diabetes_130_us_hospitals_for_years_1999_2008.data.targets 
+    y = diabetes_130_us_hospitals_for_years_1999_2008.data.targets
+    dataset = diabetes_130_us_hospitals_for_years_1999_2008.data.original
     
     # metadata 
     print(diabetes_130_us_hospitals_for_years_1999_2008.metadata)
@@ -17,13 +18,12 @@ def main():
     print(diabetes_130_us_hospitals_for_years_1999_2008.variables)
     variable_info = diabetes_130_us_hospitals_for_years_1999_2008.variables
 
-    return X, y, metadata, variable_info
+    return dataset, metadata, variable_info
 
 if __name__ == "__main__":
-    X, y, metadata, variable_info = main()
+    dataset, metadata, variable_info = main()
 
-    X.to_csv("data/raw/features.csv", index=False)
-    y.to_csv("data/raw/targets.csv", index=False)
+    dataset.to_csv("data/raw/dataset.csv", index=False)
 
     with open("data/raw/metadata.json", "w") as f:
         json.dump(metadata, f, indent=4)
